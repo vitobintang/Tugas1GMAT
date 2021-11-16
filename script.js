871d544816c030481d24a7c7c5e7860eabf5a4a5
vitobintang <vitoapik@gmail.com>
2021-11-16 22:49:41 +0700

    First Try

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let data = [];
-console.log(data);
+const text = '460462_Delavito Bintang Mahaputra_Tugas Kalkulus 7.pdf';
+const arr = text.split('_');
 
+const arr2 = arr[2].split('.');
+
+const data = {
+    niu: Number(arr[0]),
+    nama: arr[1],
+    judul: arr2[0],
+    ekstensi: arr2[1],
+};
+
+console.log(data)
